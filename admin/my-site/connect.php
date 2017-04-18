95e9486fac6110df53a47a3e0f90dd317d190cd9
873056154@qq.com <CHUXK>
2017-04-18 18:20:53 +0800

    1

--- admin/my-site/connect.php
+++
@@ -1,5 +0,0 @@
-<?php
-	header("Content-type: text/json;charset:utf-8;Access-Control-Allow-Origin:*");
-	$con = mysqli_connect('localhost','root','','bishe') or die('连接失败啦');
-	mysqli_query($con,'set names utf8')
-?>
--- admin/my-site/loginChange.php
+++
@@ -1,8 +0,0 @@
-<?php
-	require_once ('connect.php');
-	$new = file_get_contents("php://input");
-	$sql = "UPDATE `adminroot` SET `pas` = ".$new." WHERE `adminroot`.`ID` = 1";
-	$result = mysqli_query($con,$sql);
-	// 关闭数据库连接：mysql_close(连接到数据库的变量)
-	mysqli_close($con);
-?>
